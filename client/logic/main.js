6add5d51b7182965787c973e455b9b6003d6489d
Tom Newport <tom.newport@nanoporetech.com>
2018-12-20 14:54:03 +0000

    Work from Tuesday

--- client/logic/main.js
+++ client/logic/main.js
@@ -1,7 +1,65 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Moment from 'react-moment';
+
+function displayMessage(message){
+  return <section key = {message.time} className = "message">
+<h2>{message.sender}</h2>
+<h3><Moment fromNow>{message.time}</Moment></h3>
+<p>{message.message}</p>
+
+  </section>;
+}
+class MessageSender extends Component{
+  render(){return <section className = "new-message" >
+    <input id = "sender-name"></input>
+    <textarea id = "message"></textarea>
+    <button onClick = {
+      function(){
+        const sendername = document.getElementById("sender-name").value;
+        const messagetosend = document.getElementById("message").value;
+        const timemessagesent = new Date().toISOString();
+        console.log(sendername, messagetosend,timemessagesent)
+        fetch('http://tnewport-mac.oxfordnanolabs.local:8000/messages',{
+          method :"POST",
+          mode: "cors",
+          headers : {"Content-Type":"application/json"},
+          body : JSON.stringify({
+            sender:sendername,
+            message:messagetosend,
+            time:timemessagesent
+          })
+        })
+        .then(function(){
+          document.getElementById("message").value = ""
+        })
+      }
+    }>send</button>
+  </section>}
+}
+class Messages extends Component{
+  constructor(){
+    super();
+    this.state = { messages : [ ]}
+  }
+  componentDidMount(){
+    setInterval(
+    ()=>fetch('http://tnewport-mac.oxfordnanolabs.local:8000/messages', {mode: "cors"})
+      .then(response => response.json())
+        .then(newMessages => this.setState({messages : newMessages})),1000
+)
+  }
+  render(){
+    return <article>
+<section>
+    {this.state.messages.map(displayMessage)}
+</section>
+<MessageSender></MessageSender>
+    </article>;
+  }
+}
 
 export default () => {
-    const element = <p>Hello World</p>;
+    const element = <Messages></Messages>;
     ReactDOM.render(element, document.getElementById('app-root'));
 }
--- server/index.js
+++ server/index.js
@@ -1,17 +1,74 @@
-const Hapi=require('hapi');
+const Hapi = require('hapi');
 
-const server=Hapi.server({
-    host:'localhost',
+const server = Hapi.server({
+    host:'0.0.0.0',
     port:8000
 });
 
+var messages = [
+{
+  sender : "Andy",
+  time : "2018-12-18T15:21:51.437Z",
+  message : "🦎  + lizard"
+
+},
+{
+  sender : "Leo",
+  time : "2018-12-18T16:21:51.437Z",
+  message : "🐳 + whale"
+}
+ ];
+
+server.route(
+  {
+    'method': 'GET',
+    path: '/messages',
+    handler : function(){
+      return messages
+    },
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }
+    }
+  }
+)
+
+server.route(
+  {
+    'method': 'POST',
+    path: '/messages',
+    handler : function(request){
+      const newMessage = request.payload;
+      messages.push(newMessage);
+
+      return "okay"
+    },
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }
+    }
+  }
+)
 // Add a route
 server.route({
     method:'GET',
     path:'/hello',
     handler:function(request,h) {
 
-        return'hello world';
+        return 'hello Alex';
+    }
+});
+
+server.route({
+    method:'POST',
+    path:'/hello',
+    handler:function(request,h) {
+
+        return request["payload"]["hello"];
     }
 });
 
